70e6ecba86dc0ba74b7a07a8d7b4200d65f77fee
Mousty Mainer <moustymainer@gmail.com>
2023-01-05 19:36:20 +0100

    working on components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME1 from '../../assets/me1.png'
+import HeaderSocials  from './HeaderSocials'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container.header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mousty Mainer</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me" >
+          <img src={ME1} alt="me" />
+        </div>
+
+         <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
