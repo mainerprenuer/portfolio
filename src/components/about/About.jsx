06de6aa319222a16b0ca8fa4b18ad8a7c3cf5ff3
Mousty Mainer <moustymainer@gmail.com>
2023-01-07 09:29:37 +0100

    working on about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.png'
+import {FaAward} from 'react-icons/fa'
+import {BsFillPeopleFill} from 'react-icons/bs'
+import {BsFolderSymlinkFill} from 'react-icons/bs'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+    <div className='container about__container'>
+      <div className='about__me'>
+        <img src={ME} alt='About Image'/>
+      </div>
+
+      <div className='about__content'>
+        <div className='about__cards'>
+          <article className='about__card'>
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>3+ Years Working</small>
+          </article>
+
+          <article className='about__card'>
+            <BsFillPeopleFill className='about__icon'/>
+            <h5>Clients</h5>
+            <small>100+ Worldwide</small>
+          </article>
+          
+          <article className='about__card'>
+            <BsFolderSymlinkFill className='about__icon'/>
+            <h5>Projects</h5>
+            <small>37+ Completed</small>
+          </article>
+        </div>
+
+        <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem. 
+        At vero eos et accusamus. Qui officia deserunt mollit anim id est laborum.
+        Corrupti quos dolores et quas molestias excepturi sint occaecati.
+        </p>
+
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+      </div>
+    </div>
+    </section>
   )
 }  
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {FaTwitter} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com/moustymainer" target="_blank"><BsLinkedin /></a>
+        <a href="https://www.linkedin.com/in/moustymainer/" target="_blank"><BsLinkedin /></a>
         <a href="https://github.com/mainerprenuer" target="_blank"><FaGithub /></a>
         <a href="https://twitter.com/moustymainer" target="_blank"><FaTwitter /></a>
     </div>
