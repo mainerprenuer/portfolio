12d4c0da5b4138f78778a02195ee61815c8f18d3
Mousty Mainer <moustymainer@gmail.com>
2023-01-18 18:14:58 +0100

    testimonials works

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,43 @@
 import React from 'react'
 import './testimonials.css'
 import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg' 
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Muhammad Bello',
+    review: 'Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Aisha Abubakar',
+    review: 'Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function.'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Dr Hope Joseph',
+    review: 'Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function.'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Imran Umar',
+    review: 'Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function, Learning is a recursive function.'
+  }
+]
+
 
 const Testimonials = () => {
   return (
@@ -8,49 +45,26 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className='container testimonials__container'>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVTR1} alt='Avatar One' />
-          </div>
-          <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>Learning is a recursive function, Learning is a recursive function,
-             Learning is a recursive function, Learning is a recursive function, Learning is a recursive function
-             Learning is a recursive function, Learning is a recursive function,
-             </small>
-        </article>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVTR1} alt='Avatar One' />
-          </div>
-          <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>Learning is a recursive function, Learning is a recursive function,
-             Learning is a recursive function, Learning is a recursive function, Learning is a recursive function
-             Learning is a recursive function, Learning is a recursive function,
-             </small>
-        </article>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVTR1} alt='Avatar One' />
-          </div>
-          <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>Learning is a recursive function, Learning is a recursive function,
-             Learning is a recursive function, Learning is a recursive function, Learning is a recursive function
-             Learning is a recursive function, Learning is a recursive function,
-             </small>
-        </article>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVTR1} alt='Avatar One' />
-          </div>
-          <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>Learning is a recursive function, Learning is a recursive function,
-             Learning is a recursive function, Learning is a recursive function, Learning is a recursive function
-             Learning is a recursive function, Learning is a recursive function,
-             </small>
-        </article>
-       
-      </div>
+      <Swiper className='container testimonials__container'
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <SwiperSlide key={index} className='testimonial'>
+              <div className='client__avatar'>
+                <img src={avatar} />
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
