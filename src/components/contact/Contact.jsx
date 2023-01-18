9a7282b7a716b2fdd7dbc428c3a17d2f84475120
Mousty Mainer <moustymainer@gmail.com>
2023-01-18 20:46:42 +0100

    working on contacts

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './contact.css'
+import {MdMarkEmailRead} from 'react-icons/md'
+import {FaTwitter} from 'react-icons/fa'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+     <h5>Get In Touch</h5>
+     <h2>Contact Me</h2>
+      
+    <div className='container contact__container'>
+      <div className='contact__options'>
+        <article className='contact__option'>
+        <MdMarkEmailRead className='contact__option-icon'/>
+          <h4>Email</h4>
+          <h5>moustymainer@gmail.com</h5>
+          <a href='mailto:moustymainer@gmail.com' target="_blank">Send a message</a>
+        </article>
+        <article className='contact__option'>
+        <FaTwitter className='contact__option-icon'/>
+          <h4>Twitter</h4>
+          <h5>moustymainer</h5>
+          <a href='https://twitter.com/moustymainer' target="_blank">Send a message</a>
+        </article>
+        <article className='contact__option'>
+        <BsWhatsapp className='contact__option-icon'/>
+          <h4>Whatsapp</h4>
+          <h5>+2347031343181</h5>
+          <a href="https://api.whatsapp.com/send?phone=+2347031343181" target="_blank">Send a message</a>
+        </article>
+      </div>
+      {/* END OF CONTACT OPTIONS */}
+      <form action=''>
+        <input type="text" name="name" placeholder='Your Full Name' required />
+        <input type="email" name="email" placeholder='Your Email' required />
+        <textarea name='message' rows="7" placeholder="Your Message" required ></textarea>
+        <button type='submit' className='btn btn-primary'>Send Message</button>
+      </form>
+    </div>
+   </section>
   )
 }
 
