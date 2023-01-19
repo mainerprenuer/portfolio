8fb0a34de74a0a97a83198dcc03e7edc7a7ae5eb
Mousty Mainer <moustymainer@gmail.com>
2023-01-19 18:43:45 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Mousty Mainer</h1>
-        <h5 className="text-light">Front-End  Engineer</h5>
+        <h5 className="text-light">Full-stack Engineer</h5>
         <CTA />
         <HeaderSocials /> 
 
