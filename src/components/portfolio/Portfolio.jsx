2097a94e54c4d58c85f3cbcb272cc2c719172cf1
mainerprenuer <moustymainer@gmail.com>
2023-03-04 19:43:26 +0100

    commm

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
+// import IMG3 from '../../assets/portfolio3.jpg'
 import IMG5 from '../../assets/farmplug.png'
 // import IMG6 from '../../assets/portfolio1.jpg'
 
