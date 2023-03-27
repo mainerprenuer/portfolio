775d68d9077cee2ab209f8fabeecd6ca7b87eb9b
mainerprenuer <moustymainer@gmail.com>
2023-03-27 16:16:10 +0100

    fix bug

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
+import IMG5 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
-<<<<<<< HEAD
-// import IMG3 from '../../assets/portfolio3.jpg'
-import IMG5 from '../../assets/farmplug.png'
-=======
 import IMG3 from '../../assets/portfolio3.png'
-// import IMG5 from '../../assets/portfolio1.jpg'
->>>>>>> 47ccc8cad9b88bb8569a58ebd16fd56a25cd2ae5
-// import IMG6 from '../../assets/portfolio1.jpg'
 
 const Portfolio = () => {
   return (
@@ -50,10 +43,6 @@ const Portfolio = () => {
           <a href='www.malamiconnect.org' className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
-<<<<<<< HEAD
-
-=======
->>>>>>> 47ccc8cad9b88bb8569a58ebd16fd56a25cd2ae5
         {/* <article className='portfolio__item'>
           <div className='portfolio__item-image'>
     	      <img src={IMG3} alt=""/>
@@ -64,12 +53,7 @@ const Portfolio = () => {
           <a href='https://godelbackend.herokuapp.com/' className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article> */}
-<<<<<<< HEAD
-
-        {/* <article className='portfolio__item'>
-=======
         <article className='portfolio__item'>
->>>>>>> 47ccc8cad9b88bb8569a58ebd16fd56a25cd2ae5
           <div className='portfolio__item-image'>
     	      <img src={IMG1} alt=""/>
           </div>
@@ -89,10 +73,6 @@ const Portfolio = () => {
           <a href='https://apcaims.org' className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article> */}
-<<<<<<< HEAD
-        
-=======
->>>>>>> 47ccc8cad9b88bb8569a58ebd16fd56a25cd2ae5
        
       </div>
     </section>
